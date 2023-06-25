e376dcb769743c41395572ffe0b00cf031dbebef
rulentor <dvigsproject@yandex.ru>
2023-06-25 11:13:51 +0300

    'init'

---
+++ src/components/app/AppMenu.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+const model = ref([
+    {
+        label: 'Меню',
+        items: [
+		  { label: 'Главная', icon: 'pi pi-fw pi-home', to: '/' },
+		  { label: 'Заказы', icon: 'pi pi-fw pi-home', to: '/orders' },
+		  { label: 'Журнал', icon: 'pi pi-fw pi-home', to: '/ai' },
+		  { label: 'Чаты', icon: 'pi pi-fw pi-home', to: '/chat' },		  
+		]
+
+    },
+])
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
